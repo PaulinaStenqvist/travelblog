3b50c97bcad7bbc7dd90e88083d0153021330dc9
Tobias235 <55447964+Tobias235@users.noreply.github.com>
2023-10-17 20:17:48 +0200

    Fixed error in auth

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,9 +1,8 @@
-const router = require("express").Router();
-const User = require("../models/user");
-const Post = require("../models/post");
+const router = require('express').Router();
+const Post = require('../models/post');
 
 //CREATE POST
-router.post("/create", async (req, res) => {
+router.post('/create', async (req, res) => {
   const newPost = new Post(req.body);
   try {
     const savedPost = await newPost.save();
@@ -14,7 +13,7 @@ router.post("/create", async (req, res) => {
 });
 
 //UPDATE POST
-router.put("/update/:id", async (req, res) => {
+router.put('/update/:id', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post.username === req.body.username) {
@@ -31,7 +30,7 @@ router.put("/update/:id", async (req, res) => {
         res.status(500).json(err);
       }
     } else {
-      res.status(401).json("You can update only your post!");
+      res.status(401).json('You can update only your post!');
     }
   } catch (err) {
     res.status(500).json(err);
@@ -39,18 +38,18 @@ router.put("/update/:id", async (req, res) => {
 });
 
 //DELETE POST
-router.delete("/delete/:id", async (req, res) => {
+router.delete('/delete/:id', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post.username === req.body.username) {
       try {
         await post.delete();
-        res.status(200).json("Post has been deleted...");
+        res.status(200).json('Post has been deleted...');
       } catch (err) {
         res.status(500).json(err);
       }
     } else {
-      res.status(401).json("You can delete only your post!");
+      res.status(401).json('You can delete only your post!');
     }
   } catch (err) {
     res.status(500).json(err);
@@ -58,7 +57,7 @@ router.delete("/delete/:id", async (req, res) => {
 });
 
 //GET POST
-router.get("/get/:id", async (req, res) => {
+router.get('/get/:id', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     res.status(200).json(post);
@@ -68,10 +67,10 @@ router.get("/get/:id", async (req, res) => {
 });
 
 //GET ALL POSTS
-router.get("/getall", async (req, res) => {
+router.get('/getall', async (req, res) => {
   const username = req.query.user;
   const catName = req.query.cat;
-  
+
   try {
     let posts;
     if (username) {
